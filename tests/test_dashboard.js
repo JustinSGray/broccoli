61b64de0672a69adf4210ccfac071102ff24cfd3
Justin Gray <justin.s.gray@gmail.com>
2014-03-14 20:57:18 -0400

    project tests started

--- tests/test_dashboard.js
+++ tests/test_dashboard.js
@@ -1,22 +1,47 @@
 
 var assert = require('assert');
 
+
+function setup(client, cb) {
+  client.eval(function() {
+
+    function cb() {
+      emit('userCreated');
+    }
+
+    Accounts.createUser({username:'test', password: '123456'}, cb);
+  });
+}
+
 suite('Project functionality', function() {
   test('Error msg for no name', function(done, server, client) {
-    client.eval(function() {
-      waitForDOM('#new-project-form', function(){
-        var form = $('#new-project-form');
-        var formGroup = form.find('.form-group'); 
 
-        // starts without error class, no error msg
-        assert.equal(formGroup.hasClass('has-error'), false);
-        assert.equal(form.find('.help-block').text(), "");
+    setup(client);
 
-        form.find('button').click() //empty form
-        emit('submitted');
-        
+    client.once('userCreated', function(){ 
+      client.eval(function(){
+        waitForDOM('#new-project-form', function(){
+          console.log('Test');
+          var form = $('#new-project-form');
+          var formGroup = form.find('.form-group'); 
+    
+          // starts without error class, no error msg
+          emit('hasClass',formGroup.hasClass('has-error'));
+          emit('helpText', form.find('.help-block').text());
+
+          form.find('button').click() //empty form
+          emit('submitted');
+          emit('return');
+        });
+      }).once('hasClass', function(hasClass){
+        assert.equal(hasClass, false);
+      }).once('helpText', function(helpText){
+        assert.equal(helpText, '');
       });
-    }).once('submitted', function() {
+      
+    });
+
+    client.once('submitted', function() {
       // should get error class and report error message
       assert.equal(formGroup.hasClass('has-error'), true);
       assert.equal(form.find('.help-block').text(), "Provide a name for the project.");
